c0957c351acbcedafa5281f710c1d207be493ba9
IshaqKD <ishak1582002@gmail.com>
2024-08-24 20:55:15 +0100

    finished changes

---
+++ src/views/EventDetailsView.vue
@@ -0,0 +1,31 @@
+<script setup>
+import axios from 'axios'
+import { ref, onMounted } from 'vue'
+import EventService from '../services/EventService'
+
+const props = defineProps({
+  id: {
+    required: true,
+  }
+})
+
+const event = ref(null)
+
+onMounted(() => {
+  EventService.getEvent(props.id)
+    .then((res) => {
+      event.value = res.data
+      console.log(res)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+})
+</script>
+<template>
+  <div v-if="event">
+    <h1>{{ event.title }}</h1>
+    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+    <p>{{ event.description }}</p>
+  </div>
+</template>
