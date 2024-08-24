96489dc65e9058124d493437ce2c26bc1664a23d
IshaqKD <ishak1582002@gmail.com>
2024-08-24 19:16:21 +0100

    completed changes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,40 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import EventCard from '@/components/EventCard.vue'
+import { ref } from 'vue';
+
+const events = ref([
+  {
+    id: 112,
+    title: 'Cat Adoption Date',
+    time: '12:00',
+    date: 'march 12, 2021',
+  },
+  {
+    id: 113,
+    title: 'Cat Adoption Date',
+    time: '12:00',
+    date: 'march 12, 2021',
+  },
+  {
+    id: 114,
+    title: 'Cat Adoption Date',
+    time: '12:00',
+    date: 'march 12, 2021',
+  },
+])
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Events For Learning</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :event="event" :key="event.id"></EventCard>
+  </div>
 </template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
